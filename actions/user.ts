990c1248ac575cc439aebff71a7e9241ef68ab44
bmo4401 <haiminh612a@gmail.com>
2023-10-03 13:21:50 +0700

    deploy

--- actions/user.ts
+++ actions/user.ts
@@ -1,56 +1,68 @@
 import { FormatReceive, RegisterUserReceive, UserReceive } from '@/type';
 import {
-   REFRESH_USER_ROUTE,
-   CHECK_USER_ROUTE,
-   ONBOARD_USER_ROUTE,
+  REFRESH_USER_ROUTE,
+  CHECK_USER_ROUTE,
+  ONBOARD_USER_ROUTE,
 } from '@/utils/ApiRoutes';
 import axios from 'axios';
 import { StaticImageData } from 'next/image';
 export const checkUser = async (email: string) => {
-   const {
+  try {
+    const {
       data: { data },
-   } = await axios.post<FormatReceive<UserReceive>>(
+    } = await axios.post<FormatReceive<UserReceive>>(
       CHECK_USER_ROUTE,
       {
-         email,
+        email,
       },
-      { withCredentials: true },
-   );
-   return data;
+      {
+        withCredentials: true,
+        headers: {
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Control-Allow-Credentials': 'true',
+        },
+      },
+    );
+    console.log('❄️ ~ file: user.ts:12 ~ data:', data);
+    return data;
+  } catch (err: any) {
+    console.log(err);
+    return err?.response?.data;
+  }
 };
 
 export const refresh = async () => {
-   try {
-      const {
-         data: { data },
-      } = await axios.get<FormatReceive<undefined>>(
-         REFRESH_USER_ROUTE,
+  try {
+    const {
+      data: { data },
+    } = await axios.get<FormatReceive<undefined>>(
+      REFRESH_USER_ROUTE,
 
-         { withCredentials: true },
-      );
-      return data;
-   } catch (err: any) {
-      return err?.response?.data;
-   }
+      { withCredentials: true },
+    );
+    return data;
+  } catch (err: any) {
+    return err?.response?.data;
+  }
 };
 
 export const register = async (
-   email: string,
-   name: string,
-   profileImage: string | StaticImageData,
-   about: string,
+  email: string,
+  name: string,
+  profileImage: string | StaticImageData,
+  about: string,
 ) => {
-   const {
-      data: { data },
-   } = await axios.post<FormatReceive<RegisterUserReceive>>(
-      ONBOARD_USER_ROUTE,
-      {
-         email,
-         name,
-         profileImage,
-         about,
-      },
-      { withCredentials: true },
-   );
-   return data;
+  const {
+    data: { data },
+  } = await axios.post<FormatReceive<RegisterUserReceive>>(
+    ONBOARD_USER_ROUTE,
+    {
+      email,
+      name,
+      profileImage,
+      about,
+    },
+    { withCredentials: true },
+  );
+  return data;
 };
